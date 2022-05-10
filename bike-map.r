96fb2e4288abecb6f73e3afb20db18eac0b7256e
rcozmyer <51971787+rcozmyer@users.noreply.github.com>
2022-05-09 21:20:37 -0500

    Works!
    It's crude, but it works. Pulls in a folder with a mixture of gpx and/or fit files and gives you a plot that year's weekly group rides!

--- bike-map.r
+++ bike-map.r
@@ -15,11 +15,12 @@ library(ggmap)
 
 #### Read fit files####
 # TODO make this a function
-fileslist <- list.files("./testdata", pattern = "\\.fit$", full.names = TRUE)
+fileslistfit <- list.files("./TTT_2021", pattern = "\\.fit$", full.names = TRUE)
+fileslistgpx <- list.files("./TTT_2021", pattern = "\\.gpx$", full.names = TRUE)
 
 first = 0
-for (f in fileslist){
-  fname <- basename(f)
+for (f in fileslistfit){
+  # Maybe extract the filename as the ID for use in the feature?
   rideCoords <- records(readFitFile(f)) %>%
     bind_rows() %>% 
     arrange(timestamp) %>%
@@ -27,18 +28,18 @@ for (f in fileslist){
   
   # This will remove any rows that are all NA from the dataframe
   rideCoords[rowSums(is.na(rideCoords)) != ncol(rideCoords),]
-  #Converts the dataframe to a line object
+  # Converts the dataframe to a line object
   TTTLine <- st_as_sf(x=rideCoords[rowSums(is.na(rideCoords)) != ncol(rideCoords),],
                      coords = c("position_long", "position_lat"),
                      crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0") %>%
     summarise(do_union = FALSE) %>%
     st_cast("LINESTRING")
   if (first == 0){
-    yearlyTTT <- TTTLine
+    yearlyTTTfit <- TTTLine
     first = 1
   }
   else{
-    yearlyTTTfit <- st_union(TTTLine, yearlyTTTfir)
+    yearlyTTTfit <- st_union(TTTLine, yearlyTTTfit)
   }
 }
 
@@ -46,12 +47,26 @@ plot(yearlyTTTfit)
 
 #### Read gpx files####
 # TODO write a function to read gpx files
+first = 0
+for (f in fileslistgpx){
+  # Maybe extract the filename as the ID for use in the feature?
+  st_layers(f)
+  TTTLine <- st_read(f, layer = "tracks")
 
+  if (first == 0){
+    yearlyTTTgpx <- TTTLine$"geometry"
+    first = 1
+  }
+  else{
+    yearlyTTTgpx <- st_union(TTTLine$"geometry", yearlyTTTgpx)
+  }
+}
+plot(yearlyTTTgpx)
 
 #### Combine the gpx and fit rides ####
 # TODO combine the gpx and fit ride lines
-#yearlyTTT <- st_union(yearlyTTTgpx, yearlyTTTfit)
-yearlyTTT <- yearlyTTTfit
+yearlyTTT <- st_union(yearlyTTTgpx, yearlyTTTfit)
+#yearlyTTT <- yearlyTTTfit
 
 #### Map the Data ####
 ggplot() +
